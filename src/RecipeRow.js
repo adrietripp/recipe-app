c91e4948536ff0ac43533eb71b2eccfee7f6fa9e
Adrianna Campos <adriejnr@gmail.com>
2025-08-25 22:08:13 -0400

    Fix image display issue

--- src/RecipeRow.js
+++ src/RecipeRow.js
@@ -6,7 +6,16 @@ function RecipeRow({ recipe, index, deleteRecipe }) {
       <td>{recipe.name}</td>
       <td>{recipe.cuisine}</td>
       <td>
-        <img src={recipe.photo} alt={`${recipe.name}`} style={{ objectFit: "scale-down", width: "100%", height: "100px" }} />
+        <img
+          src={recipe.photo}
+          alt={recipe.name}
+          style={{
+            objectFit: "cover",   // Ensures the full image fits nicely
+            width: "100%",        // Fills the entire cell width
+            height: "100px",      // Keeps all images uniform
+            display: "block"      // Removes spacing below images
+          }}
+        />
       </td>
       <td className="content_td"><p>{recipe.ingredients}</p></td>
       <td className="content_td"><p>{recipe.preparation}</p></td>
